ff52531c06bc2bd8ff10e8d0f057f789c2150d81
Esosa Orumwese <eorumwese@gmail.com>
2024-07-22 13:58:33 +0100

    added dataloader functions that don't implement cross validation as I don't want that anymore

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 ## Contains utility functions for the 3 different models
+import numpy as np
 from torch.utils.data import Dataset, DataLoader
 from sklearn.model_selection import StratifiedKFold, train_test_split
 from src.dataset import TransportModeDataset, FeatureMapDataset, CombinedDataset
@@ -8,7 +9,25 @@ class BiLSTM:
       def __init__(self) -> None:
             pass
 
-      def create_dataloaders(self, features, labels, k=5, batch_size=16, seed=42):
+      def create_dataloaders(self, features, labels, batch_size=16, seed=42):
+            '''Create training and validation dataloaders for training'''
+            
+            train_features, val_features, train_labels, val_labels = train_test_split(features, labels, test_size=0.2, 
+                                                                  stratify=labels, random_state=seed)
+            
+            dataloaders = []
+
+            train_dataset = TransportModeDataset(train_features, train_labels)
+            validation_dataset = TransportModeDataset(val_features, val_labels)
+
+            train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+            validation_loader = DataLoader(validation_dataset, batch_size=batch_size, shuffle=True)
+
+            dataloaders.append((train_loader, validation_loader))
+            
+            return dataloaders
+
+      def create_dataloaders_CV(self, features, labels, k=5, batch_size=16, seed=42):
             '''Create training and validation dataloaders for training with kfold CV'''
             
             skf = StratifiedKFold(n_splits=k, shuffle=True, random_state=seed)
@@ -33,7 +52,25 @@ class ResNet50_GRU:
       def __init__(self) -> None:
             pass
 
-      def create_dataloaders(self, features, labels, k=5, batch_size=16, seed=42):
+      def create_dataloaders(self, features, labels, batch_size=16, seed=42):
+            '''Create training and validation dataloaders for training'''
+            
+            train_features, val_features, train_labels, val_labels = train_test_split(features, labels, test_size=0.2, 
+                                                                  stratify=labels, random_state=seed)
+            
+            dataloaders = []
+
+            train_dataset = FeatureMapDataset(train_features, train_labels)
+            validation_dataset = FeatureMapDataset(val_features, val_labels)
+
+            train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+            validation_loader = DataLoader(validation_dataset, batch_size=batch_size, shuffle=True)
+
+            dataloaders.append((train_loader, validation_loader))
+            
+            return dataloaders
+
+      def create_dataloaders_CV(self, features, labels, k=5, batch_size=16, seed=42):
             '''Create training and validation dataloaders for training with kfold CV'''
             
             skf = StratifiedKFold(n_splits=k, shuffle=True, random_state=seed)
@@ -58,8 +95,31 @@ class MultiTaskModel:
       def __init__(self) -> None:
             pass
 
-      def create_combined_dataloaders(self, sequences, seq_labels, feature_maps, fmap_labels, k=5, batch_size=16, random_seed=42):
-            '''Create dataloaders out of the combined dataset'''
+      def create_combined_dataloaders(self, sequences, seq_labels, feature_maps, fmap_labels, batch_size=16, seed=42):
+            '''Create training and validation dataloaders for training'''
+
+            # stratifying by fmap_labels because that one has 3 classes while seq_labels has 2
+            train_idx, val_idx = train_test_split(np.arange(len(fmap_labels)), test_size=0.2, random_state=seed, stratify=fmap_labels)
+            
+            dataloaders = []
+
+            train_sequences, val_sequences = sequences[train_idx], sequences[val_idx]
+            train_seq_labels, val_seq_labels = seq_labels[train_idx], seq_labels[val_idx]
+            train_feature_maps, val_feature_maps = feature_maps[train_idx], feature_maps[val_idx]
+            train_fmap_labels, val_fmap_labels = fmap_labels[train_idx], fmap_labels[val_idx]
+
+            train_dataset = CombinedDataset(train_sequences, train_seq_labels, train_feature_maps, train_fmap_labels)
+            validation_dataset = CombinedDataset(val_sequences, val_seq_labels, val_feature_maps, val_fmap_labels)
+
+            train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, drop_last=False)
+            validation_loader = DataLoader(validation_dataset, batch_size=batch_size, shuffle=True, drop_last=False)
+
+            dataloaders.append((train_loader, validation_loader))
+            
+            return dataloaders
+
+      def create_combined_dataloaders_CV(self, sequences, seq_labels, feature_maps, fmap_labels, k=5, batch_size=16, random_seed=42):
+            '''Create dataloaders out of the combined dataset using stratified KFOld'''
             skf = StratifiedKFold(n_splits=k, shuffle=True, random_state=random_seed)
             dataloaders = []
 
